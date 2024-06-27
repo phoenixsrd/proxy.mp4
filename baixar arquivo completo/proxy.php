0c4d24850de0353f1682e2ff8ff6a1cfb751ed2d
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-06-27 15:12:29 -0300

    Update proxy.php

--- baixar arquivo completo/proxy.php
+++ baixar arquivo completo/proxy.php
@@ -36,3 +36,28 @@
 
 </body>
 </html>
+<?php
+// Verifica Se A URL Do Vídeo Foi Fornecida
+if (!isset($_GET['url'])) {
+    die('URL Do Vídeo Não Especificada.');
+}
+
+// URL Do Vídeo
+$videoUrl = $_GET['url'];
+
+// Função Para Buscar O Conteúdo Da URL Alvo
+function fetchURL($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+// Obtém O Conteúdo Da URL Alvo Via Proxy
+$content = fetchURL($videoUrl);
+
+// Exibe O Conteúdo Como Resposta
+echo $content;
+?>
