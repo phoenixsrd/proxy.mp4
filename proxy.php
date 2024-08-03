61cc596b6727305dfcc1eb9cf35821ebd0eca722
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-08-03 05:11:29 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,13 +1,10 @@
 <?php
-// Verifica Se A URL Do Vídeo Foi Fornecida
-if (!isset($_GET['url'])) {
+if (!isset($_GET['//pomf2.lain.la/f/wy0tf11h.mp4'])) {
     die('URL Do Vídeo Não Especificada.');
 }
 
-// URL Do Vídeo
 $videoUrl = $_GET['//pomf2.lain.la/f/wy0tf11h.mp4'];
 
-// Função Para Buscar O Conteúdo Da URL Alvo
 function fetchURL($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -17,9 +14,7 @@ function fetchURL($url) {
     return $response;
 }
 
-// Obtém O Conteúdo Da URL Alvo Via Proxy
 $content = fetchURL($videoUrl);
 
-// Exibe O Conteúdo Como Resposta
 echo $content;
 ?>
